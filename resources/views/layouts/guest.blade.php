6c7b887fbb56c62f9f1e610c46c3a18a1122ad2e
johnacumalaka <rafliilham234@gmail.com>
2025-11-21 17:38:28 +0700

    Breeze + Sneat template

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,41 +1,91 @@
 <!DOCTYPE html>
-<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default">
-<head>
-    <!-- META + CSS Sneat -->
-    <link rel="stylesheet" href="/assets/vendor/css/core.css">
-    <link rel="stylesheet" href="/assets/vendor/css/theme-default.css">
-    <link rel="stylesheet" href="/assets/css/demo.css">
-    <link rel="stylesheet" href="/assets/vendor/css/pages/page-auth.css">
+<html
+  lang="en"
+  class="light-style customizer-hide"
+  dir="ltr"
+  data-theme="theme-default"
+  data-assets-path="assets/"
+  data-template="vertical-menu-template-free"
+>
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <meta name="description" content="" />
+
+    <!-- Sneat Favicon -->
+    <link rel="icon" type="image/x-icon" href="/assets/img/favicon/favicon.ico" />
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="/assets/vendor/fonts/boxicons.css" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="/assets/vendor/css/core.css" />
+    <link rel="stylesheet" href="/assets/vendor/css/theme-default.css" />
+    <link rel="stylesheet" href="/assets/css/demo.css" />
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="/assets/vendor/css/pages/page-auth.css" />
+
+    <!-- Helpers -->
+    <script src="/assets/vendor/js/helpers.js"></script>
+    <script src="/assets/js/config.js"></script>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
+  </head>
 
-<body>
-  <div class="container-xxl">
-    <div class="authentication-wrapper authentication-basic container-p-y">
-      <div class="authentication-inner">
+  <body>
+    <div class="container-xxl">
+      <div class="authentication-wrapper authentication-basic container-p-y">
+        <div class="authentication-inner">
+          <!-- Card -->
+          <div class="card">
+            <div class="card-body">
 
-        <div class="card">
-          <div class="card-body">
+              <!-- Logo -->
+              <div class="app-brand justify-content-center mb-4">
+                <a href="/" class="app-brand-link gap-2">
+                  <span class="app-brand-logo demo">
+                    <img src="/assets/img/logo.png" alt="Logo" width="40">
+                  </span>
+                  <span class="app-brand-text demo text-body fw-bolder">Sneat</span>
+                </a>
+              </div>
 
-            <!-- logo sneat -->
-            <div class="app-brand justify-content-center">
-                ...logo sneat...
+              <!-- SLOT BREEZE -->
+              {{ $slot }}
+
             </div>
-
-            <!-- FORM LOGIN/REGISTER MUNCUL DI SINI -->
-            {{ $slot }}
-
           </div>
+          <!-- /Card -->
         </div>
-
       </div>
     </div>
-  </div>
 
-  <!-- JS Sneat -->
-  <script src="/assets/vendor/libs/jquery/jquery.js"></script>
-  <script src="/assets/vendor/js/bootstrap.js"></script>
-  <script src="/assets/js/main.js"></script>
-</body>
+    <!-- Core JS -->
+    <script src="/assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="/assets/vendor/libs/popper/popper.js"></script>
+    <script src="/assets/vendor/js/bootstrap.js"></script>
+    <script src="/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="/assets/vendor/js/menu.js"></script>
+
+    <!-- Main JS -->
+    <script src="/assets/js/main.js"></script>
+
+  </body>
 </html>
 
